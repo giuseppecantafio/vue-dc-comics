430bec5643fc0847ed2205ed2d1d4c204bbf212d
giuseppecantafio <giuseppecantafio@hotmail.it>
2022-05-04 14:42:11 +0200

    aggiunti stili e AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,25 @@
 <template>
   <div id="app">
-    <HelloWorld/>
+    <AppHeader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppHeader
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import './style/generals';
+@import './style/variables';
+
+.debug{
+  border: 1px solid black;
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+ <div class="debug">ciao</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
