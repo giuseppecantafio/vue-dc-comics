b9d00672116543736b27db159ef8b24b50726012
giuseppecantafio <giuseppecantafio@hotmail.it>
2022-05-05 19:22:37 +0200

    aggiunto 'slider' e bottoni

--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <div class="jumbotron"></div>
+    <div class="jumbotron">
+      <span class="button">Current series</span>
+    </div>
   </div>
 </template>
 
@@ -16,6 +18,7 @@ export default {
 <style scoped lang="scss">
 @import "../style/variables";
 @import "../style/generals";
+
 .jumbotron{
   margin-top: 3px;
   background-image: url("../assets/img/jumbotron.jpg");
@@ -23,6 +26,15 @@ export default {
   background-size: cover;
   height: 400px;
   width: 100%;
+  position: relative;
 
+  .button {
+  @include jb-main-buttons;
+  height: 50px;
+  font-size: 1.5rem;
+  position: absolute;
+  bottom: -25px;
+  left: 155px;
+  }
 }
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,7 +2,13 @@
   <div>
     <main>
       <div class="main-wrapper">
-        <h1>--&gt; Content goes here &lt;--</h1>
+        <div class="boxes" v-for="(card, index) in cards" :key="index">
+          <div class="img-wrapper"><img :src="`${card.thumb}`" :alt="`${card.series}`"></div>
+          <div class="text-container"><span>{{card.series}}</span></div>
+        </div>
+      </div>
+      <div class="button-container">
+        <span class="button">Load more</span>
       </div>
     </main>
   </div>
@@ -12,27 +18,149 @@
 export default {
   name: "AppMain",
   data() {
-    return {};
-  },
+    return {
+      cards: [
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+          price: "$19.99",
+          series: "Action Comics",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+          price: "$3.99",
+          series: "American Vampire 1976",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+          price: "$16.99",
+          series: "Aquaman",
+          type: "graphic novel",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+          price: "$2.99",
+          series: "Batgirl",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+          price: "$3.99",
+          series: "Batman",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+          price: "$2.99",
+          series: "Batman Beyond",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+          price: "$3.99",
+          series: "Batman/Superman",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
+          price: "$4.99",
+          series: "Batman/Superman Annual",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+          price: "$5.99",
+          series: "Batman: The Joker War Zone",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+          price: "$6.99",
+          series: "Batman: Three Jokers",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+          price: "$4.99",
+          series: "Batman: White Knight Presents: Harley Quinn",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
+          price: "$16.99",
+          series: "Catwoman",
+          type: "graphic novel",
+        },
+      ]
+    };
+  }
 };
 </script>
 
 <style scoped lang="scss">
 @import "../style/variables";
+@import "../style/generals";
+
 main {
-  height: 150px;
+  padding-top: 20px;
+  height: 620px;
   background-color: $bg-main;
   width: 100%;
 
   .main-wrapper {
+    padding-left: 20px;
+    height: 520px;
     width: 80%;
     margin: 0 auto;
+    display: flex;
+   justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    
   }
-  h1 {
+  .boxes{
+    width: calc(100% / 6);
+  }
+  .img-wrapper{
+    height: 180px;
+    width: 180px;
+  }
+  img{
+    object-fit: cover;
     height: 100%;
-    line-height: 150px;
-    vertical-align: middle;
-    color: $light-color;
+    width: 100%;
+  }
+  .text-container{
+    padding-top: 15px;
+    height: 20px;
+  }
+  span{
+    font-size: 0.8rem;
+    color: white;
+    text-transform: uppercase;
+  }
+}
+.button-container{
+  height: 80px;
+  padding-top: 30px;
+  width: 100%;
+  text-align: center;
+
+  .button {
+    @include jb-main-buttons;
   }
 }
 </style>
